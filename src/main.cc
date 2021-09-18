6881e124895de504842460a5f6924a47432ca8d2
adamstarkey22 <adamstarkey22@gmail.com>
2021-09-19 00:18:34 +1200

    Implemented basic SFML window

--- src/main.cc
+++ src/main.cc
@@ -3,5 +3,19 @@
 
 int main(int argc, char** argv)
 {
+    sf::RenderWindow window(sf::VideoMode(960, 540), "mtac");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            window.clear(sf::Color::Black);
+            window.display();
+        }
+    }
+
     return 0;
 }
